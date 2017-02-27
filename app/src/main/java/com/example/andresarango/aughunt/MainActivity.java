670fa918cb4545b7db19c9831c72e95b10d8ca83
cito125 <cito125@gmail.com>
2017-02-27 00:38:22 -0500

    removed materila camera, trying camera fragment

--- app/src/main/java/com/example/andresarango/aughunt/MainActivity.java
+++ app/src/main/java/com/example/andresarango/aughunt/MainActivity.java
@@ -1,40 +1,18 @@
 package com.example.andresarango.aughunt;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.afollestad.materialcamera.MaterialCamera;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private static final int CAMERA_RQ = 6969;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new MaterialCamera(this)
-                .stillShot()
-                .start(CAMERA_RQ);
+
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        // Received recording or error from MaterialCamera
-        if (requestCode == CAMERA_RQ) {
-
-            if (resultCode == RESULT_OK) {
-                Toast.makeText(this, "Saved to: " + data.getDataString(), Toast.LENGTH_LONG).show();
-            } else if(data != null) {
-                Exception e = (Exception) data.getSerializableExtra(MaterialCamera.ERROR_EXTRA);
-                e.printStackTrace();
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-            }
-        }
-    }
 }
