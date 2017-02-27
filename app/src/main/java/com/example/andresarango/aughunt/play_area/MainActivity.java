293f726846396b0b9e24599a75c2bf1d41cf8567
Mila2010 <liudmila.ura@gmail.com>
2017-02-27 17:37:33 -0500

    workflow till HomePage

--- app/src/main/java/com/example/andresarango/aughunt/play_area/MainActivity.java
+++ app/src/main/java/com/example/andresarango/aughunt/play_area/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.andresarango.aughunt;
+package com.example.andresarango.aughunt.play_area;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.andresarango.aughunt.R;
 
 
 public class MainActivity extends AppCompatActivity {
