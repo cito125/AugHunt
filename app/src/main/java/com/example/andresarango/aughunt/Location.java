64c99656991cd6b3f2fbebba4734b81eee1befc1
cito125 <cito125@gmail.com>
2017-02-26 23:00:15 -0500

    added material camera library

---
+++ app/src/main/java/com/example/andresarango/aughunt/Location.java
@@ -0,0 +1,35 @@
+package com.example.andresarango.aughunt;
+
+/**
+ * Created by andresarango on 2/26/17.
+ */
+
+public class Location {
+    private Double lat;
+    private Double lng;
+    private Double elevation;
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    public Double getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(Double elevation) {
+        this.elevation = elevation;
+    }
+}
