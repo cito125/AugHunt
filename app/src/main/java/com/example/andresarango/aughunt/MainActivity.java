72358492b13c60ff5d45bf1cd82486c0562b3ec1
cito125 <cito125@gmail.com>
2017-02-27 00:57:44 -0500

    removed camera fragment library

--- app/src/main/java/com/example/andresarango/aughunt/MainActivity.java
+++ app/src/main/java/com/example/andresarango/aughunt/MainActivity.java
@@ -1,18 +1,36 @@
 package com.example.andresarango.aughunt;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.florent37.camerafragment.CameraFragment;
+import com.github.florent37.camerafragment.configuration.Configuration;
+import com.github.florent37.camerafragment.internal.controller.view.CameraView;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private CameraView mCameraView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCameraView = (CameraView) findViewById(R.id.camera);
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 }
