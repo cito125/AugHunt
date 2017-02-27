a9282522dbed056caa772303b4d3b9b52f38ba08
Mila2010 <liudmila.ura@gmail.com>
2017-02-27 17:38:05 -0500

    workflow till HomePage

--- app/src/main/java/com/example/andresarango/aughunt/login/CreateAccountFragment.java
+++ app/src/main/java/com/example/andresarango/aughunt/login/CreateAccountFragment.java
@@ -1,8 +1,10 @@
 package com.example.andresarango.aughunt.login;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by Millochka on 2/27/17.
  */
 
-public class CreateAccountFragment {
+public class CreateAccountFragment extends Fragment {
 }
--- app/src/main/java/com/example/andresarango/aughunt/login/StartActivity.java
+++ app/src/main/java/com/example/andresarango/aughunt/login/StartActivity.java
@@ -1,7 +1,9 @@
 package com.example.andresarango.aughunt.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.andresarango.aughunt.R;
 
 public class StartActivity extends AppCompatActivity {
 
