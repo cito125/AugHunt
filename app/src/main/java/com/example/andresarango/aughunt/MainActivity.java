c2e88d2642f0d5f717587d1cf83fa3c1a47e530b
cito125 <cito125@gmail.com>
2017-02-27 11:26:00 -0500

    have camera screen showing on main activity

--- app/src/main/java/com/example/andresarango/aughunt/MainActivity.java
+++ app/src/main/java/com/example/andresarango/aughunt/MainActivity.java
@@ -1,36 +1,137 @@
 package com.example.andresarango.aughunt;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.cameraview.AspectRatio;
 import com.google.android.cameraview.CameraView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        AspectRatioFragment.Listener {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+
+    private static final String FRAGMENT_DIALOG = "dialog";
 
 
     private CameraView mCameraView;
+
+    private Handler mBackgroundHandler;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mCameraView = (CameraView) findViewById(R.id.camera);
-
-
+        if (mCameraView != null) {
+            mCameraView.addCallback(mCallback);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mCameraView.start();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            mCameraView.start();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_PERMISSION);
+        }
     }
 
     @Override
     protected void onPause() {
-        super.onPause();
         mCameraView.stop();
-
+        super.onPause();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBackgroundHandler != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                mBackgroundHandler.getLooper().quitSafely();
+            } else {
+                mBackgroundHandler.getLooper().quit();
+            }
+            mBackgroundHandler = null;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_PERMISSION:
+                if (permissions.length != 1 || grantResults.length != 1) {
+                    throw new RuntimeException("Error on requesting camera permission.");
+                }
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                }
+                // No need to start camera here; it is handled by onResume
+                break;
+        }
+    }
+
+
+
+    @Override
+    public void onAspectRatioSelected(@NonNull AspectRatio ratio) {
+        if (mCameraView != null) {
+
+        }
+    }
+
+    private Handler getBackgroundHandler() {
+        if (mBackgroundHandler == null) {
+            HandlerThread thread = new HandlerThread("background");
+            thread.start();
+            mBackgroundHandler = new Handler(thread.getLooper());
+        }
+        return mBackgroundHandler;
+    }
+
+    private CameraView.Callback mCallback
+            = new CameraView.Callback() {
+
+        @Override
+        public void onCameraOpened(CameraView cameraView) {
+            System.out.println("camera opened");
+        }
+
+        @Override
+        public void onCameraClosed(CameraView cameraView) {
+            System.out.println("camera closed");
+        }
+
+        @Override
+        public void onPictureTaken(CameraView cameraView, final byte[] data) {
+            System.out.println("picture taken");
+
+        }
+
+    };
+
+
 }
