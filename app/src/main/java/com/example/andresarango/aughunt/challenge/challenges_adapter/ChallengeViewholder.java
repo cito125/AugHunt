ac05f76e2806264c109eb95eea2b2dc5dae4a585
cito125 <cito125@gmail.com>
2017-03-05 21:58:54 -0500

    make viewholder clickable

--- app/src/main/java/com/example/andresarango/aughunt/challenge/challenges_adapter/ChallengeViewholder.java
+++ app/src/main/java/com/example/andresarango/aughunt/challenge/challenges_adapter/ChallengeViewholder.java
@@ -1,20 +1,35 @@
 package com.example.andresarango.aughunt.challenge.challenges_adapter;
 
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.andresarango.aughunt.R;
 import com.example.andresarango.aughunt.challenge.Challenge;
+import com.example.andresarango.aughunt.challenge.ChallengeActivity;
 
-class ChallengeViewholder<T> extends RecyclerView.ViewHolder {
+class ChallengeViewholder<T> extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private final TextView mHintTextView;
+    private final ImageView mChallengeImageView;
+
     public ChallengeViewholder(View itemView) {
         super(itemView);
-        itemView.findViewById(R.id.viewholder_challenge_hint);
-        itemView.findViewById(R.id.viewholder_challenge_picture);
+        mHintTextView = (TextView) itemView.findViewById(R.id.viewholder_challenge_hint);
+        mChallengeImageView = (ImageView) itemView.findViewById(R.id.viewholder_challenge_picture);
     }
 
     public void bind(Challenge<T> challenge) {
+        mHintTextView.setText(challenge.getHint());
+        itemView.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        
     }
 }
