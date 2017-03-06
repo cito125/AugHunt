4f29e90dedcd90e56240c6d3b4ef9c3a1e8898c8
cito125 <cito125@gmail.com>
2017-03-06 14:30:25 -0500

    removed location checker class, put methods in location class instead

--- app/src/main/java/com/example/andresarango/aughunt/challenge/ChallengeFilter.java
+++ app/src/main/java/com/example/andresarango/aughunt/challenge/ChallengeFilter.java
@@ -1,7 +1,6 @@
 package com.example.andresarango.aughunt.challenge;
 
 import com.example.andresarango.aughunt.location.Location;
-import com.example.andresarango.aughunt.location.LocationChecker;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,14 +9,9 @@ public class ChallengeFilter<T> {
 
     public List<Challenge<T>> filterChallengesByProximity(List<Challenge<T>> mChallengeList, Location userLocation, Double radius) {
         List<Challenge<T>> nearbyChallenges = new ArrayList<>();
-        LocationChecker locationChecker = new LocationChecker();
-
         for (int i = 0; i < mChallengeList.size(); i++) {
             Challenge<T> challenge = mChallengeList.get(i);
-            boolean challengeIsNearUser = locationChecker.areLocationsWithinRadius(
-                    userLocation,
-                    challenge.getLocation(),
-                    radius);
+            boolean challengeIsNearUser = userLocation.isWithinRadius(challenge.getLocation(),radius);
             if(challengeIsNearUser){
                 nearbyChallenges.add(challenge);
             }
--- app/src/main/java/com/example/andresarango/aughunt/location/Location.java
+++ app/src/main/java/com/example/andresarango/aughunt/location/Location.java
@@ -37,4 +37,55 @@ public class Location {
     public void setElevation(Double elevation) {
         this.mElevation = elevation;
     }
+
+    public boolean isWithinRadius(Location secondLocation, Double radius){
+        return isWithinRadius(getLat(), getLng(), getElevation(),
+                secondLocation.getLat(),
+                secondLocation.getLng(),
+                secondLocation.getElevation(),
+                radius);
+    }
+
+    public Double distanceTo(Location secondLocation){
+        return getDistance(getLat(), getLng(), getElevation(),
+                secondLocation.getLat(),
+                secondLocation.getLng(),
+                secondLocation.getElevation());
+    }
+
+    private boolean isWithinRadius(Double firstLat,
+                                   Double firstLng,
+                                   Double firstHeight,
+                                   Double secondLat,
+                                   Double secondLng,
+                                   Double secondHeight,
+                                   Double radius) {
+
+        double distance = getDistance(firstLat, firstLng, firstHeight, secondLat, secondLng, secondHeight);
+
+        return distance <= radius;
+    }
+
+    private double getDistance(Double firstLat,
+                               Double firstLng,
+                               Double firstHeight,
+                               Double secondLat,
+                               Double secondLng,
+                               Double secondHeight) {
+        final Double R = 6371.0; // Radius of the earth in km
+
+        Double latDistance = Math.toRadians(firstLat - secondLat);
+        Double lonDistance = Math.toRadians(firstLng - secondLng);
+        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(secondLat)) * Math.cos(Math.toRadians(firstLat))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        Double distance = R * c * 1000; // convert to meters
+
+        Double height = secondHeight - firstHeight;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance); // returns distance in meters
+    }
 }
