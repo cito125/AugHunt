2ec436b3539c289778aafd15fb4b90102d28c0f3
dannylui91 <dannylui91@gmail.com>
2017-03-08 23:30:16 -0500

    Added needed libraries and created challenge photo and completed class

---
+++ app/src/main/java/com/example/andresarango/aughunt/challenge/ChallengePhoto.java
@@ -0,0 +1,74 @@
+package com.example.andresarango.aughunt.challenge;
+
+import com.example.andresarango.aughunt.location.DAMLocation;
+
+import java.util.List;
+
+/**
+ * Created by dannylui on 3/8/17.
+ */
+
+public class ChallengePhoto {
+    private String challengeId;
+    private String ownerId;
+    private DAMLocation location;
+    private String photoUrl;
+    private String hint;
+    private int pursuing;
+    private List<ChallengePhotoCompleted> completedPhotoChallenges;
+    private long timestamp;
+
+    public ChallengePhoto() {
+
+    }
+
+    public ChallengePhoto(String challengeId, String ownerId, DAMLocation location, String photoUrl, String hint, long timestamp) {
+        this.challengeId = challengeId;
+        this.ownerId = ownerId;
+        this.location = location;
+        this.photoUrl = photoUrl;
+        this.hint = hint;
+        this.timestamp = timestamp;
+        this.pursuing = 0;
+    }
+
+    public String getChallengeId() {
+        return challengeId;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public DAMLocation getLocation() {
+        return location;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public int getPursuing() {
+        return pursuing;
+    }
+
+    public List<ChallengePhotoCompleted> getCompletedPhotoChallenges() {
+        return completedPhotoChallenges;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setPursuing(int pursuing) {
+        this.pursuing = pursuing;
+    }
+
+    public void setCompletedPhotoChallenges(List<ChallengePhotoCompleted> completedPhotoChallenges) {
+        this.completedPhotoChallenges = completedPhotoChallenges;
+    }
+}
---
+++ app/src/main/java/com/example/andresarango/aughunt/challenge/ChallengePhotoCompleted.java
@@ -0,0 +1,15 @@
+package com.example.andresarango.aughunt.challenge;
+
+/**
+ * Created by dannylui on 3/8/17.
+ */
+
+public class ChallengePhotoCompleted {
+    private String challengeId;
+    private String ownerId;
+
+    public ChallengePhotoCompleted(String challengeId, String ownerId) {
+        this.challengeId = challengeId;
+        this.ownerId = ownerId;
+    }
+}
