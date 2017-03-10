dab2bed657db779d24efeb731983a78f35bc06ee
Danny Lui <dannylui91@gmail.com>
2017-03-10 15:06:19 -0500

    Added splash, login design

--- app/src/main/java/com/example/andresarango/aughunt/login/SplashScreen.java
+++ app/src/main/java/com/example/andresarango/aughunt/login/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.andresarango.aughunt.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.andresarango.aughunt.R;
@@ -11,5 +13,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("SPLASHING WAITING... 5s");
+                startActivity(new Intent(SplashScreen.this, StartActivity.class));
+                finish();
+            }
+        }, 5000);
     }
 }
