44c8f09948caa41ddd327e5256e008a1ae0d9f88
Danny Lui <dannylui91@gmail.com>
2017-03-10 11:45:58 -0500

    Deleted firebase emulator and helper, working on user object and putting it into firebase

--- app/src/main/java/com/example/andresarango/aughunt/user/User.java
+++ app/src/main/java/com/example/andresarango/aughunt/user/User.java
@@ -5,21 +5,43 @@ package com.example.andresarango.aughunt.user;
  */
 
 public class User {
+    private String userId;
+    private String profileName;
+    private int userPoints;
+    private String profilePicUrl;
 
-    private final String mUserId;
-    private Integer mUserPoints;
 
-    public User(){
-
-        this.mUserId="Mila";
+    public User(String userId, String profileName) {
+        this.userId = userId;
+        this.profileName = profileName;
+        this.userPoints = 0;
     }
 
     public String getUserId() {
-        return mUserId;
+        return userId;
     }
 
-    public void setmUserPoints(Integer mUserPoints) {
-        this.mUserPoints = mUserPoints;
+    public int getUserPoints() {
+        return userPoints;
     }
 
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public String getProfilePicUrl() {
+        return profilePicUrl;
+    }
+
+    public void setUserPoints(int userPoints) {
+        this.userPoints = userPoints;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public void setProfilePicUrl(String profilePicUrl) {
+        this.profilePicUrl = profilePicUrl;
+    }
 }
