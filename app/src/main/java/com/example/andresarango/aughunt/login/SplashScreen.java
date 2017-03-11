f2864298e3e1d351f5213619c0fe81eb7cfa05b8
dannylui91 <dannylui91@gmail.com>
2017-03-10 19:08:51 -0500

    Fixed splash screen and comments mentioned

--- app/src/main/java/com/example/andresarango/aughunt/login/SplashScreen.java
+++ app/src/main/java/com/example/andresarango/aughunt/login/SplashScreen.java
@@ -8,21 +8,31 @@ import android.support.v7.app.AppCompatActivity;
 import com.example.andresarango.aughunt.R;
 
 public class SplashScreen extends AppCompatActivity {
+    private static final int DELAY_FIVE_SECONDS = 5000;
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            startActivity(new Intent(SplashScreen.this, StartActivity.class));
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        handler.postDelayed(runnable, DELAY_FIVE_SECONDS);
+    }
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("SPLASHING WAITING... 5s");
-                startActivity(new Intent(SplashScreen.this, StartActivity.class));
-                finish();
-            }
-        }, 5000);
+    @Override
+    protected void onStop() {
+        super.onStop();
+        handler.removeCallbacks(runnable);
     }
 }
