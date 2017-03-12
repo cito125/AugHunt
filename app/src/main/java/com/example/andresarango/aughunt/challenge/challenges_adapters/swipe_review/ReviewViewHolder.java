2967e8d8a915c24080d5555fc12ca7b83be7caa7
cito125 <cito125@gmail.com>
2017-03-12 16:17:52 -0400

    refactored abstract Swiping adapter

---
+++ app/src/main/java/com/example/andresarango/aughunt/challenge/challenges_adapters/swipe_review/ReviewViewHolder.java
@@ -0,0 +1,17 @@
+package com.example.andresarango.aughunt.challenge.challenges_adapters.swipe_review;
+
+
+import android.content.Context;
+
+import com.example.andresarango.aughunt.swipe_adapter.SwipeAdapter;
+
+class ReviewViewHolder<T> extends SwipeAdapter.ViewHolder<Integer> {
+    public ReviewViewHolder(Context context) {
+        super(context);
+    }
+
+    @Override
+    public SwipeAdapter.ViewHolder onBindViewholder(Integer object) {
+        return null;
+    }
+}
