1c366f5390b4adc9dc21fbede21977ca8969d4a5
cito125 <cito125@gmail.com>
2017-03-12 16:53:30 -0400

    refactored swiperadapter class and made ReviewSwipeAdapter

--- app/src/main/java/com/example/andresarango/aughunt/challenge/challenges_adapters/swipe_review/ReviewSwipeAdapter.java
+++ app/src/main/java/com/example/andresarango/aughunt/challenge/challenges_adapters/swipe_review/ReviewSwipeAdapter.java
@@ -1,29 +1,33 @@
 package com.example.andresarango.aughunt.challenge.challenges_adapters.swipe_review;
 
 
-import android.view.View;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.example.andresarango.aughunt.R;
 import com.example.andresarango.aughunt.swipe_adapter.SwipeAdapter;
 
 public class ReviewSwipeAdapter extends SwipeAdapter<Integer, ReviewViewHolder<Integer>> {
-    @Override
-    public ReviewViewHolder onCreateViewholder(int position, ViewHolder viewHolder, ViewGroup viewGroup) {
-        return null;
-    }
+
 
     @Override
     public int getCount() {
-        return 0;
+        return  Integer.MAX_VALUE;
     }
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public ReviewViewHolder<Integer> onCreateViewholder(int position, ReviewViewHolder<Integer> viewHolder, ViewGroup viewGroup) {
+        if(viewHolder == null){
+            viewHolder = new ReviewViewHolder<>(
+                    LayoutInflater.from(viewGroup.getContext())
+                            .inflate(R.layout.vh_review_challenge_swipe,viewGroup,false
+                            )
+            );
+        }
+
+        return viewHolder.onBindViewholder(position);
     }
 }
