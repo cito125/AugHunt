39ca336058ea653261e4e3178de1b91533bbfeff
Danny Lui <dannylui91@gmail.com>
2017-03-17 18:43:31 -0400

    Working on profile tab

--- app/src/main/java/com/example/andresarango/aughunt/profile/viewpager/account/ProfileTabFragment.java
+++ app/src/main/java/com/example/andresarango/aughunt/profile/viewpager/account/ProfileTabFragment.java
@@ -34,6 +34,7 @@ import butterknife.ButterKnife;
 public class ProfileTabFragment extends Fragment {
     @BindView(R.id.iv_main_profile_pic) ImageView profilePicIv;
     @BindView(R.id.tv_main_profile_name) TextView profileNameTv;
+    @BindView(R.id.tv_main_profile_user_id) TextView profileUserIdTv;
     @BindView(R.id.tv_main_profile_points) TextView userPointsTv;
     @BindView(R.id.tv_main_profile_total_created) TextView totalCreatedChallengesTv;
     @BindView(R.id.tv_main_profile_total_submitted) TextView totalSubmittedChallengesTv;
@@ -44,7 +45,7 @@ public class ProfileTabFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tab_profile, container, false);
+        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
     }
 
     @Override
@@ -75,7 +76,8 @@ public class ProfileTabFragment extends Fragment {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 User currentUser = dataSnapshot.getValue(User.class);
                 profileNameTv.setText(currentUser.getProfileName());
-                userPointsTv.setText(String.valueOf(currentUser.getUserPoints()));
+                profileUserIdTv.setText("ID: " + auth.getCurrentUser().getUid());
+                userPointsTv.setText(currentUser.getUserPoints() + "/100 PTS");
                 totalCreatedChallengesTv.setText(String.valueOf(currentUser.getNumberOfCreatedChallenges()));
                 totalSubmittedChallengesTv.setText(String.valueOf(currentUser.getNumberOfSubmittedChallenges()));
             }
