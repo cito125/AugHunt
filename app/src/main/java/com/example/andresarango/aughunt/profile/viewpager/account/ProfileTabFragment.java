48736a1550a4cbe2ab3f404503924fc828be40ef
Danny Lui <dannylui91@gmail.com>
2017-03-18 01:09:47 -0400

    WIP profile page

--- app/src/main/java/com/example/andresarango/aughunt/profile/viewpager/account/ProfileTabFragment.java
+++ app/src/main/java/com/example/andresarango/aughunt/profile/viewpager/account/ProfileTabFragment.java
@@ -56,7 +56,7 @@ public class ProfileTabFragment extends Fragment {
         setupProfileStatusBar();
 
         Glide.with(getContext())
-                .load("http://clipart-library.com/images/rcLojMEni.jpg")
+                .load("http://ps4daily.com/wp-content/uploads/2016/02/crash-1449380161172.jpg")
                 .asBitmap()
                 .centerCrop()
                 .into(new BitmapImageViewTarget(profilePicIv) {
@@ -77,7 +77,7 @@ public class ProfileTabFragment extends Fragment {
                 User currentUser = dataSnapshot.getValue(User.class);
                 profileNameTv.setText(currentUser.getProfileName());
 //                profileUserIdTv.setText("ID: " + auth.getCurrentUser().getUid());
-                userPointsTv.setText(currentUser.getUserPoints() + "/100 PTS");
+                userPointsTv.setText("0 " + currentUser.getUserPoints() + "/100 PTS");
                 totalCreatedChallengesTv.setText(String.valueOf(currentUser.getNumberOfCreatedChallenges()));
                 totalSubmittedChallengesTv.setText(String.valueOf(currentUser.getNumberOfSubmittedChallenges()));
             }
