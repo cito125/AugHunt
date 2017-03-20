19bf93fecfa1f8536c2d679d09f5f6d60ed30ba7
cito125 <cito125@gmail.com>
2017-03-19 21:54:44 -0400

    added ARActivity

--- app/src/main/java/com/example/andresarango/aughunt/create/PictureTakenActivity.java
+++ app/src/main/java/com/example/andresarango/aughunt/create/PictureTakenActivity.java
@@ -1,16 +1,19 @@
 package com.example.andresarango.aughunt.create;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.example.andresarango.aughunt.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by dannylui on 3/18/17.
@@ -19,16 +22,21 @@ import butterknife.BindView;
 public class PictureTakenActivity extends AppCompatActivity {
     @BindView(R.id.iv_edit_photo)
     ImageView photoIv;
+
+    public static final String DESTINATION_KEY = "YOMAMA";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_taken);
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
 
-        byte[] data = getIntent().getByteArrayExtra("CAMERA_PICTURE");
+        if (intent.hasExtra(DESTINATION_KEY)) {
+            String filePath = intent.getStringExtra(DESTINATION_KEY);
+            photoIv.setImageBitmap(BitmapFactory.decodeFile(filePath));
+        }
 
-        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-
-        Glide.with(this).load(bitmap).into(photoIv);
 
     }
 }
